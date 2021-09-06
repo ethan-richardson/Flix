9203a0a2eb3099ad90ef62f28eab6b593be8064a
ethan-richardson <ethanmrichardson@gmail.com>
2021-09-05 20:46:18 -0400

    Added movie posters

--- flix/MoviesViewController.swift
+++ flix/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -44,9 +45,17 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         let synopsis = movie["overview"] as! String
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        cell.posterView.af.setImage(withURL: posterUrl)
+
+        
+        
+        
         //cell.posterView =
         return cell
     }
