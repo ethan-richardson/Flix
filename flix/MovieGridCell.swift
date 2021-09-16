0b11c5d35bb1f3b34cf1d732b948fca02aee077e
ethan-richardson <ethanmrichardson@gmail.com>
2021-09-16 19:05:35 -0400

    Added movie details and grid view tab

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Ethan Richardson on 9/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
